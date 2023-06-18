b1ef9cab01be9a029c8446b3e8d4e3a4027bea6c
강명구 <mg5566@hanmail.net>
2023-06-18 17:02:39 +0900

    [feat] create dashboard layout component
    grid layout plus 를 사용한 grid layout component 개발 및 적용

--- src/components/DashboardLayout.vue
+++ src/components/DashboardLayout.vue
@@ -1,11 +1,36 @@
 <template>
   <div class="dashboard-layout" @dragend="handleAddWidget">
     <PopoverButton />
+      <!-- Default slot usage -->
+    <GridLayout
+      v-model:layout="layout"
+    >
+      <GridItem
+        v-for="item in layout"
+        :key="item.i"
+        :x="item.x"
+        :y="item.y"
+        :w="item.w"
+        :h="item.h"
+        :i="item.i"
+      >
+        <div class="grid-item">
+          {{ item.i }}
+        </div>
+      </GridItem>
+    </GridLayout>
   </div>
 </template>
 
 <script setup lang="ts">
   import PopoverButton from './PopoverButton.vue';
+  import { reactive } from 'vue'
+  import { GridLayout, GridItem } from 'grid-layout-plus'
+
+  const layout = reactive([
+    { x: 0, y: 0, w: 2, h: 2, i: '0' },
+    { x: 2, y: 0, w: 2, h: 4, i: '1' }
+  ])
 
   const handleAddWidget = (event: DragEvent) => {
     console.log('위젯을 추가한다.');
@@ -17,5 +42,17 @@
   .dashboard-layout {
     width: 100%;
     height: 100%;
+  }
+
+  .grid-item {
+    color: black;
+    background-color: #ccc;
+
+    width: 100%;
+    height: 100%;
+    /* text 가 가운데에 정렬된다. */
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 </style>
