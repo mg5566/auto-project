c43b56c72678bcef6553df91e3721e384ed4f46f
강명구 <mg5566@hanmail.net>
2023-06-21 18:45:26 +0900

    [feat] create drag from outside
    외부에서 grid item 을 추가하는 예제 완성

---
+++ src/components/DragFromOutside.vue
@@ -0,0 +1,196 @@
+<script setup lang="ts">
+import { ref, onMounted, onBeforeUnmount } from 'vue'
+// you can import from 'lodash-es' or implement it by yourself
+import { throttle } from 'lodash';
+
+import { GridLayout } from 'grid-layout-plus'
+import PopoverButton from './PopoverButton.vue';
+
+const layout = ref([
+  { x: 0, y: 0, w: 2, h: 2, i: '0' },
+  { x: 2, y: 0, w: 2, h: 4, i: '1' },
+  { x: 4, y: 0, w: 2, h: 5, i: '2' },
+  { x: 6, y: 0, w: 2, h: 3, i: '3' },
+  { x: 8, y: 0, w: 2, h: 3, i: '4' },
+])
+
+const wrapper = ref<HTMLElement>()
+const gridLayout = ref<InstanceType<typeof GridLayout>>()
+
+onMounted(() => {
+  document.addEventListener('dragover', syncMousePosition)
+})
+
+onBeforeUnmount(() => {
+  document.removeEventListener('dragover', syncMousePosition)
+})
+
+const mouseAt = { x: -1, y: -1 }
+
+function syncMousePosition(event: MouseEvent) {
+  mouseAt.x = event.clientX
+  mouseAt.y = event.clientY
+}
+
+const dropId = 'drop'
+const dragItem = { x: -1, y: -1, w: 2, h: 2, i: '' }
+
+const drag = throttle(() => {
+  const parentRect = wrapper.value?.getBoundingClientRect()
+
+  if (!parentRect || !gridLayout.value) return
+
+  const mouseInGrid =
+    mouseAt.x > parentRect.left &&
+    mouseAt.x < parentRect.right &&
+    mouseAt.y > parentRect.top &&
+    mouseAt.y < parentRect.bottom
+
+  if (mouseInGrid && !layout.value.find(item => item.i === dropId)) {
+    layout.value.push({
+      x: (layout.value.length * 2) % 12,
+      y: layout.value.length + 12, // puts it at the bottom
+      w: 2,
+      h: 2,
+      i: dropId
+    })
+  }
+
+  const index = layout.value.findIndex(item => item.i === dropId)
+
+  if (index !== -1) {
+    const item = gridLayout.value.getItem(dropId)
+
+    if (!item) return
+
+    try {
+      item.wrapper.style.display = 'none'
+    } catch (e) {}
+
+    Object.assign(item.state, {
+      top: mouseAt.y - parentRect.top,
+      left: mouseAt.x - parentRect.left
+    })
+    const newPos = item.calcXY(mouseAt.y - parentRect.top, mouseAt.x - parentRect.left)
+
+    if (mouseInGrid) {
+      gridLayout.value.dragEvent('dragstart', dropId, newPos.x, newPos.y, dragItem.h, dragItem.w)
+      dragItem.i = String(index)
+      dragItem.x = layout.value[index].x
+      dragItem.y = layout.value[index].y
+    } else {
+      gridLayout.value.dragEvent('dragend', dropId, newPos.x, newPos.y, dragItem.h, dragItem.w)
+      layout.value = layout.value.filter(item => item.i !== dropId)
+    }
+  }
+})
+
+function dragEnd() {
+  const parentRect = wrapper.value?.getBoundingClientRect()
+
+  if (!parentRect || !gridLayout.value) return
+
+  const mouseInGrid =
+    mouseAt.x > parentRect.left &&
+    mouseAt.x < parentRect.right &&
+    mouseAt.y > parentRect.top &&
+    mouseAt.y < parentRect.bottom
+
+  if (mouseInGrid) {
+    alert(`Dropped element props:\n${JSON.stringify(dragItem, ['x', 'y', 'w', 'h', 'i'], 2)}`)
+    gridLayout.value.dragEvent('dragend', dropId, dragItem.x, dragItem.y, dragItem.h, dragItem.w)
+    layout.value = layout.value.filter(item => item.i !== dropId)
+  } else {
+    return
+  }
+
+  layout.value.push({
+    x: dragItem.x,
+    y: dragItem.y,
+    w: dragItem.w,
+    h: dragItem.h,
+    i: dragItem.i
+  })
+  gridLayout.value.dragEvent('dragend', dragItem.i, dragItem.x, dragItem.y, dragItem.h, dragItem.w)
+
+  const item = gridLayout.value.getItem(dropId)
+
+  if (!item) return
+
+  try {
+    item.wrapper.style.display = ''
+  } catch (e) {}
+}
+</script>
+
+<template>
+  <div class="layout-json">
+    Displayed as <code>[x, y, w, h]</code>:
+    <div class="columns">
+      <div v-for="item in layout" :key="item.i" class="layout-item">
+        <b>{{ item.i }}</b>: [{{ item.x }}, {{ item.y }}, {{ item.w }}, {{ item.h }}]
+      </div>
+    </div>
+  </div>
+  <br />
+  <PopoverButton
+    @drag-for-add-widget="drag"
+    @drag-end="dragEnd"
+  />
+  <div ref="wrapper">
+    <GridLayout ref="gridLayout" v-model:layout="layout" :row-height="30">
+      <template #item="{ item }">
+        <span class="text">{{ item.i }}</span>
+      </template>
+    </GridLayout>
+  </div>
+</template>
+
+<style scoped>
+.vgl-layout {
+  background-color: #eee;
+}
+
+:deep(.vgl-item:not(.vgl-item--placeholder)) {
+  background-color: #ccc;
+  border: 1px solid black;
+}
+
+:deep(.vgl-item--resizing) {
+  opacity: 90%;
+}
+
+:deep(.vgl-item--static) {
+  background-color: #cce;
+}
+
+.text {
+  position: absolute;
+  inset: 0;
+  width: 100%;
+  height: 100%;
+  margin: auto;
+  font-size: 24px;
+  text-align: center;
+}
+
+.layout-json {
+  padding: 10px;
+  margin-top: 10px;
+  background-color: #ddd;
+  border: 1px solid black;
+}
+
+.columns {
+  columns: 120px;
+}
+
+.droppable-element {
+  width: 150px;
+  padding: 10px;
+  margin: 10px 0;
+  text-align: center;
+  background-color: #fdd;
+  border: 1px solid black;
+}
+</style>
