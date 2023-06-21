f471e8f37bd51f934746a595f8363caba70815d1
강명구 <mg5566@hanmail.net>
2023-06-21 11:20:03 +0900

    [feat] add type
    type 추가

--- src/components/DashboardLayout.vue
+++ src/components/DashboardLayout.vue
@@ -4,6 +4,7 @@
       <!-- Default slot usage -->
     <GridLayout
       v-model:layout="layout"
+      @layout-updated="handleUpdateLayout"
     >
       <GridItem
         v-for="item in layout"
@@ -25,9 +26,9 @@
 <script setup lang="ts">
   import PopoverButton from './PopoverButton.vue';
   import { reactive } from 'vue'
-  import { GridLayout, GridItem } from 'grid-layout-plus'
+  import { GridLayout, GridItem, Layout } from 'grid-layout-plus'
 
-  const layout = reactive([
+  const layout = reactive<Layout>([
     { x: 0, y: 0, w: 2, h: 2, i: '0' },
     { x: 2, y: 0, w: 2, h: 4, i: '1' }
   ])
@@ -35,6 +36,9 @@
   const handleAddWidget = (event: DragEvent) => {
     console.log('위젯을 추가한다.');
     console.log('드래그 종료 이벤트', event.clientX, event.clientY);
+  }
+  const handleUpdateLayout = (event: Layout) => {
+    console.log('update 가 됩니다.', event);
   }
 </script>
 
