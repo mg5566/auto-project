f5f6b8666e092007d0bb8d363d261d2f86863e89
강명구 <mg5566@hanmail.net>
2023-06-23 14:48:30 +0900

    [fix] remove comments
    불필요한 주석 제거

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,10 @@
 <template>
   <div class="app-container">
-    <!-- <DashboardLayout /> -->
-    <!-- <DragFromOutside /> -->
     <RouterView />
   </div>
 </template>
 
 <script setup lang="ts">
-  // import DashboardLayout from './components/DashboardLayout.vue';
-  // import DragFromOutside from './components/DragFromOutside.vue';
 </script>
 
 <style scoped>
