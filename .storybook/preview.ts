70be5cb8b8285275d27c8161fa9cc7462a447bdf
강명구 <mg5566@hanmail.net>
2023-06-27 12:48:03 +0900

    [feat] add router into storybook
    storybook 에서 route 관련 기능이 문제없이 동작하도록 추가

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,9 +2,11 @@ import type { Preview } from "@storybook/vue3";
 import { setup } from '@storybook/vue3'; //I use Vue3
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
+import { router } from '../src/router'
 
 setup((app) => {
-  app.use(ElementPlus)
+  app.use(ElementPlus);
+  app.use(router);
 });
 
 const preview: Preview = {
