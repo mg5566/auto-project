fed8b6dd2c0d2cc755f149233be38f5637c4fdb1
강명구 <mg5566@hanmail.net>
2023-06-28 20:41:04 +0900

    [feat] create divider component
    수평 구분자 component 추가

---
+++ src/withTS/components/Divider/Divider.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="divider"></div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+  interface Props {
+    margin?: number;
+    height?: number;
+  }
+  const props = withDefaults(defineProps<Props>(), {
+    margin: 1,
+    height: 2,
+  });
+
+  const margin = computed(() => `${props.margin}px`);
+  const height = computed(() => `${props.height}px`)
+</script>
+
+<style scoped lang="scss">
+  .divider {
+    width: 100%;
+    height: 1px;
+    background-color: black;
+
+    margin: v-bind(margin) 0;
+    height: v-bind(height);
+  }
+</style>
