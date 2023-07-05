9fe47d4a4475fe5890217dc9d7fdffe2aead28f7
강명구 <mg5566@hanmail.net>
2023-07-05 17:38:31 +0900

    [feat] add vue query plugin

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import { createPinia } from 'pinia'
 import { worker } from './mocks/browser'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 
 const app = createApp(App);
 
@@ -14,6 +15,7 @@ const pinia = createPinia();
 app.use(ElementPlus);
 app.use(router);
 app.use(pinia);
+app.use(VueQueryPlugin);
 
 worker.start();
 
