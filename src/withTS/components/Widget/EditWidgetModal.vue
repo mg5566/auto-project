c902fda70191313a0d9a53a2239f53345180db25
강명구 <mg5566@hanmail.net>
2023-07-05 13:39:23 +0900

    [feat] create EditWidgetModal component
    widget 설정을 할 수 있는 edit widget modal 구현 완료

---
+++ src/withTS/components/Widget/EditWidgetModal.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-dialog v-model="isOpen">
+    <div>test {{ selectedWidget }}</div>
+  </el-dialog>
+</template>
+
+<script setup lang="ts">
+  import { computed } from 'vue';
+
+  interface Props {
+    modelValue: boolean;
+    selectedWidget: string;
+  }
+  const props = defineProps<Props>();
+  interface Emits {
+    (e: 'update:modelValue', value: boolean): void;
+  }
+  const emit = defineEmits<Emits>();
+
+  const isOpen = computed({
+    get() {
+      return props.modelValue;
+    },
+    set(value) {
+      emit('update:modelValue', value);
+    }
+  })
+</script>
+
+<style scoped lang="scss">
+</style>
