2425dda72bbe25a40b01c426ed7c2da2589314b7
강명구 <mg5566@hanmail.net>
2023-07-05 17:39:26 +0900

    [feat] create handlers
    vue query 를 사용한 panel panels api 구현
    handler 에 추가

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,10 +2,17 @@ import { rest } from 'msw';
 import PanelList from '../withTS/components/Dashboard/PanelList.json';
 
 export const handlers = [
-  rest.get('/panels', (req, res, ctx) => {
+  rest.get('/panels', (_, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.json(PanelList),
     );
   }),
+  rest.get('/panel/:panelId', (req, res, ctx) => {
+    const { panelId } = req.params;
+    return res(
+      ctx.status(200),
+      ctx.json(PanelList.find((panel) => panel.panelId === panelId)),
+    );
+  })
 ]
