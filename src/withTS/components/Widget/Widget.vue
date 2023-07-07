39c8dc20ae3b42fef356820f957e7557aceefeaa
강명구 <mg5566@hanmail.net>
2023-07-07 18:39:47 +0900

    [fix] remove edit widget
    edit widget 기능 제거

--- src/withTS/components/Widget/Widget.vue
+++ src/withTS/components/Widget/Widget.vue
@@ -1,15 +1,12 @@
 <template>
-  <!-- widget 은 header 영역과 body 영역으로 나뉨 -->
   <div class="widget">
     <!-- header -->
     <div class="widget__header">
       <div>{{ wigetTitle }}</div>
-      <a class="widget__header__edit" @click="handleOpenEditor">edit</a>
     </div>
     <!-- body -->
     <div class="widget__body">
       <!-- TODO: HOC 적용하기 -->
-      <!-- <TestPage /> -->
       <GridTable v-loading="isLoading" :tableData="tableData" />
     </div>
   </div>
@@ -17,38 +14,24 @@
 
 <script setup lang="ts">
   import { computed } from 'vue';
-  // import TestPage from '../../../pages/TestPage.vue'
   import { usePanel } from '../../../services/usePanel';
   import GridTable from '../table/GridTable/GridTable.vue';
   import { TableData } from '../table/GridTable/GridTable';
 
   interface Props {
-    // TODO: widget id 만 받아서 fetching 을 진행하자
     widgetId: string;
   }
   const props = defineProps<Props>();
 
-  interface Emits {
-    (e: 'openWidgetEditor', id: string): void;
-  }
-  const emit = defineEmits<Emits>();
+  // interface Emits {
+  //   (e: 'openWidgetEditor', id: string): void;
+  // }
+  // const emit = defineEmits<Emits>();
 
-  // const title = computed(() => {
-  //   return props.widgetId;
-  // });
-  /*
-  const data = computed(() => {
-    // TODO: type 에 따른 widget 이 생길 수 있도록 변경하기
-    // 나중에 type 에 따른 data 를 전달해야합니다.
-    // return props.widget.data;
-    return props.widgetId;
-  });
-  */
-
-  const handleOpenEditor = () => {
-    console.log('click edit');
-    emit('openWidgetEditor', props.widgetId);
-  };
+  // const handleOpenEditor = () => {
+  //   console.log('click edit');
+  //   emit('openWidgetEditor', props.widgetId);
+  // };
 
   /**
    * fetch Panel
@@ -63,7 +46,6 @@
     const tempData = data.value ?? { columns: [], rows: [] } as TableData;
     return tempData;
   })
-
 </script>
 
 <style scoped lang="scss">
