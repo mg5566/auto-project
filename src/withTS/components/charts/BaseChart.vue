5f1405cf8d76653b52fdf0573c0d689bca890574
강명구 <mg5566@hanmail.net>
2023-07-11 17:16:10 +0900

    [feat] create base chart component
    echart 의 기본 구성을 위한 component

---
+++ src/withTS/components/charts/BaseChart.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-chart
+    class="echart"
+    :option="option"
+    :autoresize="true"
+  />
+</template>
+
+<script setup lang="ts">
+  import { use } from 'echarts/core';
+  import { CanvasRenderer } from 'echarts/renderers';
+  import { PieChart } from 'echarts/charts';
+  import {
+    TitleComponent,
+    TooltipComponent,
+    LegendComponent,
+  } from 'echarts/components';
+  import { EChartsOption } from 'echarts';
+  import VChart from 'vue-echarts';
+
+  interface Props {
+    option: EChartsOption;
+  }
+  defineProps<Props>();
+
+  use([
+    CanvasRenderer,
+    PieChart,
+    TitleComponent,
+    TooltipComponent,
+    LegendComponent,
+  ])
+</script>
+
+<style scoped lang="scss">
+  .echart {
+    width: 100%;
+    height: 100%;
+  }
+</style>
