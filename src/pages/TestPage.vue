a76dcfd58521e1de3bf370ae5e6c14c1925146bd
강명구 <mg5566@hanmail.net>
2023-07-12 18:09:04 +0900

    [fix] remove test page

--- src/pages/TestPage.vue
+++ src/pages/TestPage.vue
@@ -1,49 +1,9 @@
 <template>
   <div class="test">
-    <BaseChart :option="option" />
   </div>
 </template>
 
 <script setup lang="ts">
-  import { EChartsOption } from 'echarts';
-  import BaseChart from '../withTS/components/charts/BaseChart.vue';
-
-  const option: EChartsOption = {
-    title: {
-      text: 'test pie chart',
-      left: 'center',
-    },
-    tooltip: {
-      trigger: 'item',
-    },
-    legend: {
-      orient: 'vertical',
-      left: 'left',
-      data: ['CAR1', 'CAR2', 'CAR3', 'CAR4', 'CAR5'],
-    },
-    series: {
-      name: 'test pie chart',
-      type: 'pie',
-      data: [
-        {
-          name: 'CAR1',
-          value: 40,
-        }, {
-          name: 'CAR2',
-          value: 20,
-        }, {
-          name: 'CAR3',
-          value: 20,
-        }, {
-          name: 'CAR4',
-          value: 15,
-        }, {
-          name: 'CAR5',
-          value: 5,
-        }
-      ]
-    }
-  }
 </script>
 
 <style scoped lang="scss">
