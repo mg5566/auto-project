b47fd8f55fdef8f09a4ec4e9a617e259c78a3e5f
강명구 <mg5566@hanmail.net>
2023-07-13 16:28:46 +0900

    [feat] add types into axios api
    axios 에 type 을 적용하여 response datat 가 type 을 갖도록 함

--- src/services/usePanel.ts
+++ src/services/usePanel.ts
@@ -1,14 +1,18 @@
 import { useQuery } from "@tanstack/vue-query";
+import axios from "axios";
 import { ComputedRef } from "vue";
+import { PieChartData } from "../withTS/components/charts/PieChart.type";
+import { TableData } from "../withTS/components/table/GridTable/GridTable.type";
 
 const getPanel = (panelId: string) => async () => {
   try {
-    const response = await fetch(`/panel/${panelId}`);
-    return response.json();
+    const response = await axios.get<PieChartData | TableData>(`/panel/${panelId}`)
+    return response.data;
   } catch (error) {
     console.error(error);
   }
 };
+
 export const usePanel = (panelId: string, enabled: ComputedRef<boolean>) => {
   return useQuery({
     queryKey: ["panel", panelId],
