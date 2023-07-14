207a2b95cf73681d2451a8dd0bc2bfadec2497df
강명구 <mg5566@hanmail.net>
2023-07-14 17:54:36 +0900

    [fix] edit layout state
    layout state 에 주석 제거
    type 노출

--- src/store/useLayoutStore.ts
+++ src/store/useLayoutStore.ts
@@ -1,20 +1,13 @@
 import { LayoutItem } from "grid-layout-plus";
 import { throttle } from "lodash";
 import { defineStore } from "pinia";
-import { ref, watch } from "vue";
+import { ref } from "vue";
 import { WidgetData } from "../withTS/components/Widget/WidgetList.type";
 
-type LayoutStore = LayoutItem & WidgetData;
+export type LayoutStore = LayoutItem & WidgetData;
 
 export const useLayoutStore = defineStore('layout', () => {
-  const layout = ref<LayoutStore[]>([
-  // const layout = ref<(LayoutItem & { panelId: string})[]>([
-    // { x: 0, y: 0, w: 3, h: 4, i: '0', panelId: '0' },
-    // { x: 3, y: 0, w: 3, h: 4, i: '1', panelId: '1' },
-    // { x: 6, y: 0, w: 3, h: 5, i: '2', panelId: '2' },
-    // { x: 9, y: 0, w: 3, h: 5, i: '3', panelId: '3' },
-    // { x: 9, y: 0, w: 3, h: 6, i: '4', panelId: '4' },
-  ])
+  const layout = ref<LayoutStore[]>([])
 
   watch(layout, () => {
     console.log(layout.value)
